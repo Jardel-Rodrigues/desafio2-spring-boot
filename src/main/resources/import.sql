7b4a136e6c1ffec32f4ca79ce16f3dd5ee04c4ed
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2023-12-31 18:08:58 -0300

    Adicionando uma consulta SQL para verificar a relação entre as tabelas bloco, atividade e categoria.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,10 +9,11 @@ INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina de GitHub', 'Controle de versões de seus projetos', 50.00, 2);
 
---Query que consulta a relação entre blocos e atividades pelo ID da atividade
+--Query que consulta a relação entre BLOCO, ATIVIDADE e CATEGORIA pelo ID da ATIVIDADE
 
---SELECT B.ATIVIDADE_ID, B.FIM, B.INICIO, A.PRECO, A. DESCRICAO, A.NOME, A.ID  FROM TB_BLOCO B
+--SELECT B.ATIVIDADE_ID, B.INICIO INICIO_DA_ATIVIDADE, B.FIM AS FIM_DA_ATIVIDADE,  A.PRECO, A. DESCRICAO, A.NOME, A.ID AS ID_ATIVIDADE, C.DESCRICAO AS CATEGORIA  FROM TB_BLOCO B
 --INNER JOIN TB_ATIVIDADE A ON B.ATIVIDADE_ID = A.ID
+--INNER JOIN TB_CATEGORIA C ON A.CATEGORIA_ID = C.ID
 --WHERE A.ID = 2;
 
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2023-10-25T11:00:00Z', TIMESTAMP WITH TIME ZONE '2023-10-25T18:00:00Z', 1);
