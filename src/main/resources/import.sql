2c8f9544a5730f422713ddbf6a51527d1addff97
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2023-12-31 17:42:25 -0300

    Ajuste

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,6 +9,12 @@ INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00, 1);
 INSERT INTO tb_atividade (nome, descricao, preco, categoria_id) VALUES ('Oficina de GitHub', 'Controle de versões de seus projetos', 50.00, 2);
 
+--Query que consulta a relação entre blocos e atividades pelo ID da atividade
+
+--SELECT B.ATIVIDADE_ID, B.FIM, B.INICIO, A.PRECO, A. DESCRICAO, A.NOME, A.ID  FROM TB_BLOCO B
+--INNER JOIN TB_ATIVIDADE A ON B.ATIVIDADE_ID = A.ID
+--WHERE A.ID = 2;
+
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2023-10-25T11:00:00Z', TIMESTAMP WITH TIME ZONE '2023-10-25T18:00:00Z', 1);
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2023-11-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2023-11-25T18:00:00Z', 2);
 INSERT INTO tb_bloco (inicio, fim, atividade_id) VALUES (TIMESTAMP WITH TIME ZONE '2023-11-26T11:00:00Z', TIMESTAMP WITH TIME ZONE '2023-11-26T18:00:00Z', 2);
